fc47e47240acd04b0b05f9b800760bfeff191619
alu10701951 <alu10701951@ti3-03>
2023-03-27 17:15:03 +0200

    initial commit

---
+++ src/main/java/com/mycompany/snake/Snake.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.snake;
+
+import java.util.List;
+
+/**
+ *
+ * @author alu10701951
+ */
+public class Snake {
+    private List<Node> list;
+    private Direction direction;
+
+    public Snake(List<Node> list, Direction direction) {
+        this.list = list;
+        this.direction = direction;
+    }
+    
+    
+}
